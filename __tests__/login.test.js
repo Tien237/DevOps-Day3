165f11b9431428f916750e0477b8d14d9958b0cc
Joycee23 <tuannguyen211982@gmail.com>
2025-09-30 11:31:25 +0700

    Update deploy.yml & login.test.js

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -4,7 +4,8 @@ const app = require("../app");
 describe("App basic test", () => {
   test("GET / should return 200 and contain login page", async () => {
     const res = await request(app).get("/");
+    console.log(res.text); // debug nếu fail
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain("Đăng Nhập"); // ✅ đổi chuỗi này
+    expect(res.text).toContain("Đăng Nhập"); // kiểm tra nội dung trang login
   });
 });
